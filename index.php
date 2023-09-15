53312a94728b11db39aa3b09a9e682076f3ca4fa
Alberto Bernal <albera10@hotmail.com>
2023-09-15 01:21:31 -0600

    Se inicia el proyecto

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php 
+    include('includes/header.php');
+?>
+
+    <div class="py-5">
+        <div class="container mt5">
+            <div class="row">
+                <div class="col-md-12">
+                    <h1>Pos System in PHP to MySQL</h1>
+                    <a href="login.php" class="btn btn-primary mt-4">Login</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+
+
+
+<?php 
+    include('includes/footer.php');
+?>
+  
